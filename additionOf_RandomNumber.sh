03d0919204fd067746683507b335bec02f1a5860
Nileshlad1111 <nileshcareer1111@gmail.com>
2021-05-05 16:46:46 +0530

    Added:To addition of random number

---
+++ additionOf_RandomNumber.sh
@@ -0,0 +1,8 @@
+# ! / bin / bash-x
+
+
+	numberOne=`echo $((RANDOM%8+1))`
+	numberTwo=`echo $((RANDOM%8+1))`
+	numberOf_Addition= echo "print the addition of random number "$(($numberOne+$numberTwo))
+     ##   echo "print the addition of random number  =" $numberOf_Addition
+
